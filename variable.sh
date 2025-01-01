a6b26ee606f87f01675bbcc94019af5aa8d6f913
9948180189 <hari10.143@gmail.com>
2025-01-01 12:58:35 +0530

    variablr changes

--- variable.sh
+++ variable.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo "please enter your user name"
-read USERNAME
+read -s USERNAME
 echo "Username entered is: $USERNAME"
+
+
+echo "please enter your Password"
+read -s PASSWORD
+echo "Username entered is: $PASSWORD"
+
