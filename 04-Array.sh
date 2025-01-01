5b13d19d61180b245c3384650d769b8d07bfc89b
9948180189 <hari10.143@gmail.com>
2025-01-01 13:12:51 +0530

    Arry operations performed

---
+++ 04-Array.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# index starts from 0, size is 3.
+
+FRUITS=("APPLE" "KIWI" "ORANGE")
+
+echo "First fruit is: ${FRUITS[0]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+
+
+echo "list all fruits is: ${FRUITS[@]}"
+
