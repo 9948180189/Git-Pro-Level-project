e077ac9000d4a7554f7669e161b71dfb54516bcf
9948180189 <hari10.143@gmail.com>
2025-01-01 12:47:50 +0530

    some changes was happend

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,4 +2,4 @@
 PERSON1=Ramachandra
 PERSON2=Vishnu
 echo "${PERSON1}:: hi ${PERSON2}, how areyou"
-echo "${PERSON2}:: hello ${PERSON1} iam good. how are you ?"
+echo "${PERSON2}:: hello ${PERSON1} iam good. how are you& how is your son's ?"
